fbe253b52b1ca864b8fae04f75805506a0566e77
favour mogaji <favourmogaji3@gmail.com>
2026-05-27 18:53:19 -0500

    just the basics, workd on components

--- my-react-app/src/Header.jsx
+++ my-react-app/src/Header.jsx
@@ -4,6 +4,15 @@ function Header(){
 
         <header>
         <h1>My website</h1>
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Services</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+        <hr />
         </header>
     
     )
